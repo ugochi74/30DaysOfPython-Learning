e751d0e92bba7141b494cd68fff400bfb9e6d6f6
ugochi74 <confidenceodo4@gmail.com>
2026-07-09 13:18:22 +0100

    learning

---
+++ Day_2/add.py
@@ -0,0 +1,22 @@
+x = 5
+y = 10
+print(x + y)
+
+x = "hello"
+y = 10
+print(x, y)
+
+x = "hello"
+y = "world"
+print(x, y)
+
+x = input("Type a number:")
+y = input("Type another number:")
+sum = int(x) + int(y)
+print("the sum is:", sum)
+
+x = input("Type a text:")
+y = input("Type a number:")
+w = input("Enter another text:")
+sum = x, int(y), w
+print(sum)
---
+++ Day_2/reverse.py
@@ -0,0 +1,8 @@
+#txt = "Hello World" [::-1]
+#print(txt)
+
+def my_function(x):
+    return x[::-1]
+text = my_function("A man a plan a panal panama")
+print(text)
+
