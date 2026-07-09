ff439e7c9a2c6fae1312dc64f0adc797c1e563ad
ugochi74 <confidenceodo4@gmail.com>
2026-07-09 14:42:12 +0100

    learning

---
+++ Day_2/var.py
@@ -0,0 +1,9 @@
+name = "odo confidence"
+age = 20
+height = 5.0
+is_learning_python = True
+
+print(name, type(name))
+print(age, type(age))
+print(height, type(height))
+print(is_learning_python, type(is_learning_python))
