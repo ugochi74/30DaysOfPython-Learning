f6795c252916945708cf82a6149a5f8444e21d03
ugochi74 <confidenceodo4@gmail.com>
2026-07-17 16:32:02 +0100

    learning

---
+++ Day_3/hours-seconds.py
@@ -0,0 +1,18 @@
+def hours_to_seconds(hours):
+    return hours * 60 * 60
+    
+
+
+# Don't touch below this line
+
+
+def test(hours):
+    secs = hours_to_seconds(hours)
+    print(hours, "hours is", secs, "seconds")
+
+
+test(10)
+test(1)
+test(25)
+test(100)
+test(33)
